44a4516353b86fa56b457dbc619a5fa5c75be55a
Rafal Krol <ameotoko1+github@gmail.com>
2022-02-28 09:25:21 +0100

    Chore: test

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
-output "id" {
-  description = "After the Terraform docs: 'Identifier of the organization'"
-  value       = aws_organizations_organization.org.id
-}
-
-output "accounts" {
-  description = "After the Terraform docs: 'List of organization accounts including the master account. (...)'"
-  value       = aws_organizations_organization.org.accounts
-}
-
-output "roots" {
-  description = "After the Terraform docs: 'List of organization roots. (...)'"
-  value       = aws_organizations_organization.org.roots
-}
-
-# output "org" {
-#   description = "The whole organization object as defined in the Terraform docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/organizations_organization#attributes-reference"
-#   value       = aws_organizations_organization.org
+# output "id" {
+#   description = "After the Terraform docs: 'Identifier of the organization'"
+#   value       = aws_organizations_organization.org.id
 # }
+
+# output "accounts" {
+#   description = "After the Terraform docs: 'List of organization accounts including the master account. (...)'"
+#   value       = aws_organizations_organization.org.accounts
+# }
+
+# output "roots" {
+#   description = "After the Terraform docs: 'List of organization roots. (...)'"
+#   value       = aws_organizations_organization.org.roots
+# }
+
+output "org" {
+  description = "The whole organization object as defined in the Terraform docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/organizations_organization#attributes-reference"
+  value       = aws_organizations_organization.org
+}
