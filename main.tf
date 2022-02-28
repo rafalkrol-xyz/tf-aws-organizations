30a8647fea42d6f6b5fd49e6bb4a6ea076da1250
Rafal Krol <ameotoko1+github@gmail.com>
2022-02-28 20:36:25 +0100

    Fix: include the counts

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ resource "aws_organizations_organization" "org" {
 resource "aws_organizations_organizational_unit" "ou" {
   count = length(var.organizational_units)
 
-  name      = var.organizational_units[count.index].name
-  parent_id = var.organizational_units[count.index].parent_id != null ? var.organizational_units[count.index].parent_id : aws_organizations_organization.org.roots.0.id
+  name = var.organizational_units[count.index].name
+  # NB the below will fail, by design, if the parent_id is missing and var.create_org is set to false
+  parent_id = var.organizational_units[count.index].parent_id != null ? var.organizational_units[count.index].parent_id : aws_organizations_organization.0.org.roots.0.id
 }
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,10 @@
 output "org" {
   description = "The whole organization object as described in the Terraform docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/organizations_organization#attributes-reference"
-  value       = aws_organizations_organization.org
+  value       = var.create_org ? aws_organizations_organization.0.org : null
 }
 
-output "ou" {
-  description = "The whole Organizational Unit (OU) object as described in the Terraform docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/organizations_organizational_unit#attributes-reference"
-  value       = aws_organizations_organizational_unit.ou
+output "ou_ids" {
+  description = "The identifiers (IDs) of the Organizational Unit(s) (OU) if created"
+  type        = list(string)
+  value       = aws_organizations_organizational_unit.ou.*.id
 }
