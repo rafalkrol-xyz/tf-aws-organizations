bbd5b7c29e9e5bf775b237af16ac72911945a8b4
Rafal Krol <ameotoko1+github@gmail.com>
2023-02-11 19:50:54 +0100

    Fix: from TF v1.3. this is supported natively

--- terraform.tf
+++
@@ -1,4 +0,0 @@
-# https://www.terraform.io/docs/language/expressions/type-constraints.html#experimental-optional-object-type-attributes
-terraform {
-  experiments = [module_variable_optional_attrs]
-}
